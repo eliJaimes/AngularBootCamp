8a13cb856fadef88fc74cff373c1d87bcb46bb4a
Eli Jaimes <eli.jaimes.ramos@gmail.com>
2024-01-02 17:18:29 -0600

    Mover directiva

--- AngularBootCamp/src/app/my-fist-module/my-fist-module.module.ts
+++ AngularBootCamp/src/app/my-fist-module/my-fist-module.module.ts
@@ -1,7 +1,7 @@
 /* ••[1]••••••••••••••••••••••••• my-fist-module.module.ts •••••••••••••••••••••••••••••• */
 
 import { CommonModule } from '@angular/common';
-import { MyFistDirectiveDirective } from '../my-fist-directive.directive';
+import { MyFistDirectiveDirective } from '../directives/my-fist-directive.directive';
 import { MyFistModuleRoutingModule } from './my-fist-module-routing.module';
 import { NgModule } from '@angular/core';
 
